c675b1fbac5ef40ca7cfea665d10ef331bbb09c6
Natalia Castillo Muñoz <mnacamu@gmail.com>
2022-03-04 00:10:55 +0100

    base de datos actualizada

---
+++ basedatos/scriptMiJardin.sql
@@ -0,0 +1,130 @@
+-- MySQL Workbench Forward Engineering
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+-- -----------------------------------------------------
+-- Schema miJardin
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `miJardin` DEFAULT CHARACTER SET utf8 ;
+USE `miJardin` ;
+
+-- -----------------------------------------------------
+-- Table `mijardin`.`familia`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mijardin`.`familia` (
+  `codfam` INT(10) NOT NULL,
+  `familia` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`codfam`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+-- -----------------------------------------------------
+-- Volcado de datos para la tabla `familia`
+-- -----------------------------------------------------
+INSERT INTO `familia` (`codfam`, `familia`) VALUES
+(1, 'Actinidiaceae'),
+(2, 'Aquifoliaceae'),
+(3, 'Buxaceae'),
+(4, 'Geraniaceae'),
+(5, 'Lauraceae'),
+(6, 'Punicaceae'),
+(7, 'Punicaceae'),
+(8, 'Rosaceae'),
+(9, 'Violaceae'),
+(10, 'Zigophyllaceae');
+
+-- -----------------------------------------------------
+-- Table `mijardin`.`usuarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mijardin`.`usuarios` (
+  `codusu` INT(10) NOT NULL,
+  `usuario` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(10) NOT NULL,
+  `email` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`codusu`),
+  INDEX `usuario` (`usuario` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+-- -----------------------------------------------------
+-- Volcado de datos para la tabla `usuarios`
+-- -----------------------------------------------------
+INSERT INTO `usuarios` (`codusu`, `usuario`, `password`, `email`) VALUES
+(1, 'Admin', 'Admin', ''),
+(2, 'Colaborador', '123456', ''),
+(3, 'natalia', 'natalia', 'natalia@email.com'),
+(4, 'rafa', 'rafa', 'rafa@email.com'),
+(5, 'maria', 'maria', 'maria@email.com'),
+(6, 'usuario', 'usuario', '');
+
+-- -----------------------------------------------------
+-- Table `mijardin`.`planta`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mijardin`.`planta` (
+  `codpla` INT(10) NOT NULL,
+  `nombre` VARCHAR(50) NOT NULL,
+  `cientifico` VARCHAR(50) NOT NULL,
+  `familia` INT(11) NOT NULL,
+  PRIMARY KEY (`codpla`),
+  INDEX `fk_planta_familia1_idx` (`familia` ASC),
+  CONSTRAINT `fk_planta_familia1`
+    FOREIGN KEY (`familia`)
+    REFERENCES `mijardin`.`familia` (`codfam`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+-- -----------------------------------------------------
+-- Volcado de datos para la tabla `planta`
+-- -----------------------------------------------------
+INSERT INTO `planta` (`codpla`, `nombre`, `cientifico`, `familia`) VALUES
+(1, 'Kiwi', 'Actinidia chinensis', 1),
+(2, 'Acebo', 'Ilex aquifolium', 2),
+(3, 'Boj', 'Buxus sempervirens', 3),
+(4, 'Geranio', 'Geranium pyrenaicum', 4),
+(5, 'Laurel', 'Laurus nobilis', 5),
+(6, 'Pasión', 'Passiflora caerulea', 6),
+(7, 'Granado', 'Púnica granatum', 7),
+(8, 'Fresa', 'Fragaria', 8),
+(9, 'Almendro', 'Prunus', 8),
+(10, 'Pensamiento', 'Viola tricolor', 9);
+
+-- -----------------------------------------------------
+-- Table `mijardin`.`jardin`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `miJardin`.`jardin` (
+ `codusu` INT(10) NOT NULL,
+  `codpla` INT(10) NOT NULL,
+  PRIMARY KEY (`codusu`, `codpla`),
+  INDEX `fk_usuarios_has_planta_planta1_idx` (`codpla` ASC),
+  INDEX `fk_usuarios_has_planta_usuarios1_idx` (`codusu` ASC),
+  CONSTRAINT `fk_usuarios_has_planta_usuarios1`
+    FOREIGN KEY (`codusu`)
+    REFERENCES `mijardin`.`usuarios` (`codusu`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_usuarios_has_planta_planta1`
+    FOREIGN KEY (`codpla`)
+    REFERENCES `mijardin`.`planta` (`codpla`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+--
+-- Volcado de datos para la tabla `jardin`
+--
+
+INSERT INTO `jardin` (`codusu`, `codpla`) VALUES
+(3, 3),
+(3, 9),
+(4, 6),
+(5, 8);
+
+COMMIT;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- js/javascript.js
+++ js/javascript.js
@@ -1,52 +1,29 @@
-//añado javascript para responsive tabla vista movil
-$(document).ready(function() {
+//replicar barra navegacion
+$(function(){
+  $("#nav-placeholder").load("nav.jsp");
+});
+$(function(){
+  $("#nav-placeholder1").load("nav1.jsp");
+});
+$(function(){
+  $("#nav-placeholder2").load("nav2.jsp");
+});
 
-   // inspired by http://jsfiddle.net/arunpjohny/564Lxosz/1/
-   $('.table-responsive-stack').each(function (i) {
-      var id = $(this).attr('id');
-      //alert(id);
-      $(this).find("th").each(function(i) {
-         $('#'+id + ' td:nth-child(' + (i + 1) + ')').prepend('<span class="table-responsive-stack-thead">'+        $(this).text() + ':</span> ');
-         $('.table-responsive-stack-thead').hide();
-         
-      });
-     
-   });
-  
-$( '.table-responsive-stack' ).each(function() {
-  var thCount = $(this).find("th").length; 
-   var rowGrow = 100 / thCount + '%';
-   //console.log(rowGrow);
-   $(this).find("th, td").css('flex-basis', rowGrow);   
+//replicar header
+$(function(){
+  $("#cabecera").load("header.jsp");
 });
-     
-function flexTable(){
-   if ($(window).width() < 1168) {
-      
-   $(".table-responsive-stack").each(function (i) {
-      $(this).find(".table-responsive-stack-thead").show();
-      $(this).find('thead').hide();
-   });
-         
-   // window is less than 1168px   
-   } else {
-      
-      
-   $(".table-responsive-stack").each(function (i) {
-      $(this).find(".table-responsive-stack-thead").hide();
-      $(this).find('thead').show();
-   });
-      
-   }
-// flextable   
-}       
-flexTable();
-   
-window.onresize = function(event) {
-    flexTable();
-};
-  
-// document ready  
+
+//replicar comprobación usuario
+$(function(){
+  $("#nav-placeholder2").load("comprobarUser.jsp");
 });
+
+//replicar footer
+$(function(){
+  $("#nav-placeholder2").load("footer.jsp");
+});
+
+
 
 
